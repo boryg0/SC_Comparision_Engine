4a4247d9c350dc77bcc4b6b9fe02efbf3ebfe3d8
Eryk Borycki <129526584+boryg0@users.noreply.github.com>
2024-07-12 13:15:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,39 +8,39 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import difflib
 
-def utwory_z_folderu(dis):
+def utwory_z_folderu(dis): #FUNCTION THAT SHOWS YOUR SONGS FROM FOLDER 
     try:
         if not os.path.isdir(dis):
-            print(f"Ścieżka {dis} nie jest katalogiem.")
+            print(f"Ścieżka {dis} nie jest katalogiem.") # IT MEANS THAT THE PATH IS NOT A DIRECTORY
             return []
         pliki = os.listdir(dis)
-        pliki_bez_mp3 = [os.path.splitext(plik)[0].lower() for plik in pliki]  # TO CO W TABLICY NA MALE LITERKI
-        pliki_bez_mp3.sort()  # SORTUJE LISTE
+        pliki_bez_mp3 = [os.path.splitext(plik)[0].lower() for plik in pliki]  # EVERYTHING IS LOWER FONT AND SAVED INTO ARRAY
+        pliki_bez_mp3.sort()  # SORTING LIST
         print(f"Pliki w katalogu '{dis}': {pliki_bez_mp3}")
         return pliki_bez_mp3
 
-    except Exception as e:
-        print(f"Wystąpił błąd: {e}")
+    except Exception as e: 
+        print(f"Wystąpił błąd: {e}") # COM ABOUT ERROR
         return []
 
-def utwory_z_soundclouda(playlist_url):
+def utwory_z_soundclouda(playlist_url): #FUNCTION THAT SHOWS YOUR SONGS FROM SC
     try:
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service)
         driver.get(playlist_url)
-        time.sleep(10)  # Poczekaj na załadowanie strony
+        time.sleep(10)  # WAITING FOR THE SITE 10SEC
 
-        # Użyj WebDriverWait, aby upewnić się, że elementy są załadowane
+        # USING WEBDRIVER 
         track_elements = WebDriverWait(driver, 20).until(
             EC.presence_of_all_elements_located((By.CLASS_NAME, "trackItem__trackTitle"))
         )
 
-        titles = [element.text.lower() for element in track_elements]  # ELEMENTY W TABLICY NA MALE LITERY
-        driver.quit()  # ZAMYKANIE PRZEGLADARKI
+        titles = [element.text.lower() for element in track_elements]  # ELEMENTS IN ARRAY TO LOWER FONT VERSION
+        driver.quit()  # CLOSING BROWSER
         return titles
 
     except Exception as e:
-        print(f"Wystąpił błąd: {e}")
+        print(f"Wystąpił błąd: {e}") # COM ABOUT ERROR
         return []
 
 def znajdz_podobne(tytul, lista):
@@ -50,19 +50,19 @@ def znajdz_podobne(tytul, lista):
     else:
         return None
 
-dis = 'D:\\HARDTECHNOJAKCHUJ'
-playlist_url = 'https://on.soundcloud.com/4m9pzRz1mp42gCFr5'
-folder_songs = utwory_z_folderu(dis)
+dis = 'D:\\HARDTECHNOJAKCHUJ' # DIRECTORY PATH
+playlist_url = 'https://on.soundcloud.com/4m9pzRz1mp42gCFr5' #LINK TO YOU PUBLIC PLAYLIST AT SOUNDCLOUD
+folder_songs = utwory_z_folderu(dis) 
 soundcloud_songs = utwory_z_soundclouda(playlist_url)
 
-niepasujace_piosenki = []
+niepasujace_piosenki = [] #ARRAY TO SONGS THAT ARE NOT IN YOUR FOLDER
 
 for soundcloud_title in soundcloud_songs:
     dopasowanie = znajdz_podobne(soundcloud_title, folder_songs)
     if dopasowanie is None:
-        niepasujace_piosenki.append(soundcloud_title) # DODAJE DO LISTY TYTULY KTORE NIE ZNALAZY DOPASOWANIA W CALEJ TAB
+        niepasujace_piosenki.append(soundcloud_title) # ADDING TO ARRAY SONGS THAT ARE NOT IN YOUR PLAYLIST
 
-# Wyświetl niepasujące piosenki
+# SHOWING YOUR MISSING SONGS
 if niepasujace_piosenki:
     print("Brakujące piosenki:")
     for i, song in enumerate(niepasujace_piosenki, start=1):
